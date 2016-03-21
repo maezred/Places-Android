bc855a6d1a8b9e11b7b28b48356cb96e511b8c78
Matthew Oltendorf <matthew@oltendorf.net>
2016-03-21 10:14:26 -0700

    Using support toolbar instead.

--- app/src/main/java/net/moltendorf/places/MainActivity.java
+++ app/src/main/java/net/moltendorf/places/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.SearchManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
 
 		handleIntent(getIntent());
 	}
