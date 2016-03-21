52f17ec123e2ca31442ac62278b77b7f3922cfbc
Matthew Oltendorf <matthew@oltendorf.net>
2016-03-21 08:45:56 -0700

    Add searchable data.

--- app/src/main/java/net/moltendorf/places/MainActivity.java
+++ app/src/main/java/net/moltendorf/places/MainActivity.java
@@ -1,7 +1,12 @@
 package net.moltendorf.places;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.SearchManager;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 	private static final String TAG = "MainActivity";
@@ -10,5 +15,30 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		handleIntent(getIntent());
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		return true;
+	}
+
+	@Override
+	protected void onNewIntent(Intent intent) {
+		handleIntent(intent);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		return super.onOptionsItemSelected(item);
+	}
+
+	private void handleIntent(Intent intent) {
+		if (intent.getAction().equals(Intent.ACTION_SEARCH)) {
+			String query = intent.getStringExtra(SearchManager.QUERY);
+			Log.i(TAG, "handleIntent: Performing search for \"" + query + "\"");
+		}
 	}
 }
