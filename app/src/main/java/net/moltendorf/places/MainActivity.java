954042ca0397ea028734f7c03df8937511c3fd4e
Matthew Oltendorf <matthew@oltendorf.net>
2016-03-21 10:59:29 -0700

    Add search box (button).

--- app/src/main/java/net/moltendorf/places/MainActivity.java
+++ app/src/main/java/net/moltendorf/places/MainActivity.java
@@ -1,9 +1,11 @@
 package net.moltendorf.places;
 
 import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
@@ -24,6 +26,13 @@ public class MainActivity extends AppCompatActivity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu_main, menu);
+
+		// Associate searchable configuration with the SearchView
+		SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+		SearchView    searchView    = (SearchView) menu.findItem(R.id.action_search).getActionView();
+		searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
 
 		return true;
 	}
@@ -35,6 +44,13 @@ public class MainActivity extends AppCompatActivity {
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
+		int id = item.getItemId();
+
+		switch (id) {
+			case R.id.action_search:
+				return true;
+		}
+
 		return super.onOptionsItemSelected(item);
 	}
 
