cf12c39489755c5b2787985c433acfbf4da19815
Matthew Oltendorf <matthew@oltendorf.net>
2016-03-23 17:17:01 -0700

    Add event listener binding code.

--- app/src/main/java/net/moltendorf/places/view/PlacesListAdapter.java
+++ app/src/main/java/net/moltendorf/places/view/PlacesListAdapter.java
@@ -9,7 +9,13 @@ import android.view.ViewGroup;
 import net.moltendorf.places.Place;
 import net.moltendorf.places.R;
 
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.EventListener;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 public class PlacesListAdapter extends RecyclerView.Adapter<PlacesListAdapter.ViewHolder> {
@@ -20,6 +26,9 @@ public class PlacesListAdapter extends RecyclerView.Adapter<PlacesListAdapter.Vi
 
 	private Map<Class<?>, Integer> viewTypeLookup;
 	private Class<?>[]             viewHolderLookup; // We already guarantee it extends ViewHolder.
+
+	private List<WeakReference<ViewHolder>> holderReferences = new LinkedList<>();
+	private List<EventListener>             eventListeners   = new ArrayList<>();
 
 	public PlacesListAdapter(Context context, Map<Class<?>, Class<? extends ViewHolder>> relations, Map<Integer, Place> objects) {
 		Log.d(TAG, "PlacesListAdapter: Called.");
@@ -51,10 +60,19 @@ public class PlacesListAdapter extends RecyclerView.Adapter<PlacesListAdapter.Vi
 	public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 		try {
 			// Todo: better type safety guarantees using more generics so we don't crash here as often?
-			return viewHolderLookup[viewType]
+			ViewHolder holder = viewHolderLookup[viewType]
 				.asSubclass(ViewHolder.class)
 				.getConstructor(Context.class, ViewGroup.class)
 				.newInstance(context, parent);
+
+			for (EventListener listener : eventListeners) {
+				setEventListenerBinding("add", holder, listener);
+			}
+
+			// Used to add events after the fact.
+			holderReferences.add(new WeakReference<>(holder));
+
+			return holder;
 		} catch (final Exception exception) {
 			throw new IllegalStateException("Could not construct view holder.", exception);
 		}
@@ -68,6 +86,62 @@ public class PlacesListAdapter extends RecyclerView.Adapter<PlacesListAdapter.Vi
 	@Override
 	public int getItemCount() {
 		return objects.size();
+	}
+
+	public void addEventListener(EventListener listener) {
+		eventListeners.add(listener);
+
+		// Add event to existing view holders.
+		Iterator<WeakReference<ViewHolder>> iterator = holderReferences.iterator();
+
+		while (iterator.hasNext()) {
+			WeakReference<ViewHolder> holderReference = iterator.next();
+			ViewHolder holder = holderReference.get();
+
+			// Since we're using weak references, these can go null if they're destroyed.
+			if (holder == null) {
+				iterator.remove();
+			} else {
+				setEventListenerBinding("add", holder, listener);
+			}
+		}
+	}
+
+	public void removeEventListener(EventListener listener) {
+		eventListeners.remove(listener);
+
+		// Add event to existing view holders.
+		Iterator<WeakReference<ViewHolder>> iterator = holderReferences.iterator();
+
+		while (iterator.hasNext()) {
+			WeakReference<ViewHolder> holderReference = iterator.next();
+			ViewHolder holder = holderReference.get();
+
+			// Since we're using weak references, these can go null if they're destroyed.
+			if (holder != null) {
+				setEventListenerBinding("remove", holder, listener);
+			} else {
+				iterator.remove();
+			}
+		}
+	}
+
+	private void setEventListenerBinding(String mode, ViewHolder holder, EventListener listener) {
+		Class<?> holderClass   = holder.getClass();
+		Class<?> listenerClass = listener.getClass().getSuperclass();
+		String   name          = listenerClass.getSimpleName();
+		String   method        = mode + name;
+
+		try {
+			// This is a bad idea. Yeah. Diving right in: dynamically binding event listeners!
+			holderClass.getMethod(method, listenerClass).invoke(holder, listener);
+		} catch (Exception exception) {
+			Log.d(TAG,
+				"setEventListenerBinding: " +
+					"Class " + holderClass.getSimpleName() + " can't " + mode + " " + name + ".\n" +
+					"We are looking for: " + "public void " + method + "(" + name + " listener) { /* ... */ }"
+			);
+		}
 	}
 
 	public static abstract class ViewHolder extends RecyclerView.ViewHolder {
