c694a4bed330350f4646cf7ee38aeb26a079298b
Matthew Oltendorf <matthew@oltendorf.net>
2016-03-25 08:04:23 -0700

    Add description column to places table.

--- app/src/main/java/net/moltendorf/places/PlacesQueryHelper.java
+++ app/src/main/java/net/moltendorf/places/PlacesQueryHelper.java
@@ -19,7 +19,7 @@ public class PlacesQueryHelper extends SQLiteAssetHelper {
 	private static final String TAG = "PlacesQueryHelper";
 
 	private static final String DB_NAME    = "places_db";
-	private static final int    DB_VERSION = 4;
+	private static final int    DB_VERSION = 5;
 
 	// Tables.
 	private static final String
